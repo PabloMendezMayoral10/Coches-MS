210621893db8cf68c689a25f7c0c641090807eb5
Pablo Méndez Mayoral <pablomendezmayoral@gmail.com>
2022-12-28 15:58:11 +0100

    Config creado

---
+++ Coches-ConfigServer/src/main/java/com/practica/spring/CochesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.practica.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CochesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CochesConfigServerApplication.class, args);
+	}
+
+}
